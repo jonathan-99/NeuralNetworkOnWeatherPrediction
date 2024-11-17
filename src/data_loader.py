f335cf45f5d1d5d54b3fb2ae2c60fdb61eb4e8eb
JonathanL <jon.l@l.com>
2024-11-17 15:35:19 +0000

    error handling inconsistant input data

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,6 @@ from datetime import datetime
 # Configure logging for the module
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
 class WindSpeedData:
     def __init__(self, file_paths):
         """
@@ -60,7 +59,9 @@ class WindSpeedData:
         try:
             with open(file_path, 'r') as file:
                 for line in file:
-                    self._process_line(line, timestamps, wind_speeds, seen_timestamps)
+                    # Ignore empty lines and process only valid lines
+                    if line.strip():
+                        self._process_line(line, timestamps, wind_speeds, seen_timestamps)
         except Exception as e:
             logging.error(f"Error processing file {file_path}: {e}")
 
@@ -75,21 +76,21 @@ class WindSpeedData:
             seen_timestamps (set): The set to track unique timestamps.
         """
         try:
-            # Split the line by comma, and remove any extra spaces
-            parts = [x.strip() for x in line.strip().split(',')]
+            # Split the line by comma
+            parts = line.strip().split(',')
 
             # Ensure the line splits into exactly 2 valid values (timestamp and wind speed)
             if len(parts) != 2:
-                logging.error(f"Skipping invalid data (wrong number of values): {line.strip()}")
+                logging.warning(f"Skipping invalid data (wrong number of values): {line.strip()}")
                 return
 
             timestamp_str, wind_speed_str = parts
             # Parse the timestamp and wind speed
             try:
-                timestamp = datetime.strptime(timestamp_str, '%Y %m %d %H')
-                wind_speed = float(wind_speed_str)
-            except ValueError:
-                logging.error(f"Skipping invalid data due to value error: {line.strip()}")
+                timestamp = datetime.strptime(timestamp_str.strip(), '%Y %m %d %H')
+                wind_speed = float(wind_speed_str.strip())
+            except ValueError as ve:
+                logging.error(f"Skipping invalid data due to value error: {line.strip()} - {ve}")
                 return
 
             # Check for duplicate empty rows (timestamps with 0.0)
@@ -103,7 +104,7 @@ class WindSpeedData:
             seen_timestamps.add(timestamp)
 
         except Exception as e:
-            logging.error(f"Skipping invalid data: {line.strip()} - {e}")
+            logging.error(f"Skipping invalid data 3: {line.strip()} - {e}")
 
     def get_data(self):
         """
